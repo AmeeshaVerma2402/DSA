8a807a1813a401ac8e881f3d21c8c8cb96e76494
Ameesha Verma <ameesavermma17498@gmail.com>
2026-06-26 14:05:09 +0530

    Time: 10 ms (42.02%) | Memory: 47 MB (32.62%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,17 +1,20 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] count = new int[26];
-
-        for (char c : magazine.toCharArray()) {
-            count[c - 'a']++;
+    public boolean canConstruct(String r, String m) 
+    {
+        HashMap<Character, Integer> n= new HashMap<>();
+        for(Character c:m.toCharArray())
+        {
+                n.put(c,n.getOrDefault(c,0)+1);
         }
-
-        for (char c : ransomNote.toCharArray()) {
-            if (--count[c - 'a'] < 0) {
+        for(Character c:r.toCharArray())
+        {
+            if(!n.containsKey(c)|| n.get(c)==0)
+            {
                 return false;
             }
-        }
-
+            n.put(c,n.get(c)-1);            
+        }   
         return true;
+
     }
 }
