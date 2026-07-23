0039c998ce83fa4b56d65549ab542170cea9e4cf
Ameesha Verma <ameesavermma17498@gmail.com>
2026-07-23 19:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (31.25%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int mid=(low+high)/2;
+        while(low<=high)
+        {
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            mid=(low+high)/2;
+        }
+        return -1;
+        
+    }
+}
